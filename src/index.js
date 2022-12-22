5d189e74ce8786d2d8f4bef61951adeee222db97
Kola Kolade <kolakolade@gmail.com>
2022-12-22 10:39:07 +0100

    add code to implement add Todo to the app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+import {
+  Todo, todoForm, todoList,
+} from './modules/todo.js';
+import Views from './modules/views.js';
+import Persist from './modules/persist.js';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './css/style.css';
 import './scss/main.scss';
+
+// EVENT LISTENERS
+// On Document load
+document.addEventListener('DOMContentLoaded', Views.displayTodos);
+
+// To add a new todo
+todoForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const description = document.querySelector('.add-todo').value;
+
+  const todo = new Todo(description);
+  Views.addTodo(todo);
+  Views.clearInputField();
+  Persist.addTodoToLS(todo);
+});
