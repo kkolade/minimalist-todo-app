ceb918d48f699556866c6e8d17b01a2b8c947e28
Kola Kolade <kolakolade@gmail.com>
2023-01-06 22:09:05 +0100

    Fix linter error

--- test/add_remove.test.js
+++ test/add_remove.test.js
@@ -1,14 +1,15 @@
-import Persist from '../src/modules/persist.js';
-import 'jest-localstorage-mock';
+import Persist from "../src/modules/persist.js";
+// eslint-disable-next-line import/no-unresolved
+import "jest-localstorage-mock";
 
-describe('Add_Remove_Todos', () => {
-  describe('addTodoToLS', () => {
+describe("Add_Remove_Todos", () => {
+  describe("addTodoToLS", () => {
     beforeEach(() => {
       localStorage.clear();
     });
 
-    test('adds a new todo to local storage', () => {
-      const element = { id: 1, text: 'Visit with Grandma' };
+    test("adds a new todo to local storage", () => {
+      const element = { id: 1, text: "Visit with Grandma" };
       Persist.addTodoToLS(element);
       const todos = Persist.getLSContent();
       expect(todos[0]).toEqual(element);
@@ -16,14 +17,14 @@ describe('Add_Remove_Todos', () => {
   });
 });
 
-test('removeTodoLS removes a todo from local storage', () => {
-  const todo1 = { todoId: 1, text: 'Finish writing tests' };
-  const todo2 = { todoId: 2, text: 'Submit project' };
+test("removeTodoLS removes a todo from local storage", () => {
+  const todo1 = { todoId: 1, text: "Finish writing tests" };
+  const todo2 = { todoId: 2, text: "Submit project" };
   const todos = [todo1, todo2];
-  localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
   Persist.removeTodoLS(1);
 
-  const updatedTodos = JSON.parse(localStorage.getItem('todos'));
+  const updatedTodos = JSON.parse(localStorage.getItem("todos"));
 
-  expect(updatedTodos).toEqual([{ todoId: 1, text: 'Submit project' }]);
+  expect(updatedTodos).toEqual([{ todoId: 1, text: "Submit project" }]);
 });
